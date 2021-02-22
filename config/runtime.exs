4269d65f27474b25f395ede21e027ce43abd0ee1
Ushitora Anqou <ushitora@anqou.net>
2021-02-22 20:44:42 +0900

    Fix ffmpeg/youtubedl path

--- config/runtime.exs
+++ config/runtime.exs
@@ -5,4 +5,5 @@ config :logger, handle_sasl_reports: true
 config :nostrum,
   token: System.fetch_env!("DISCORD_TOKEN"),
   num_shards: :auto,
-  youtubedl: "/home/anqou/workspace/youtube-dl"
+  ffmpeg: "/usr/bin/ffmpeg",
+  youtubedl: "/usr/bin/youtube-dl"
