1bf3b3cf8d1d29ec2bb61f6ac49409ab30b1c684
Ushitora Anqou <ushitora@anqou.net>
2021-02-22 12:05:50 +0900

    Rename config/config.exs to config/runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -3,6 +3,6 @@ import Config
 config :logger, handle_sasl_reports: true
 
 config :nostrum,
-  token: System.get_env("DISCORD_TOKEN"),
+  token: System.fetch_env!("DISCORD_TOKEN"),
   num_shards: :auto,
   youtubedl: "/home/anqou/workspace/youtube-dl"
