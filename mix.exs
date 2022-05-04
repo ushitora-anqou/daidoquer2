119e65d77c40d8f48c90d04864f8a9046b838c4e
Ushitora Anqou <ushitora@anqou.net>
2022-05-04 22:46:47 +0900

    Bump emojix from 0.3 to ushitora-anqou/emojix:master

--- mix.exs
+++ mix.exs
@@ -34,7 +34,8 @@ defmodule Daidoquer2.MixProject do
       {:temp, "~> 0.4"},
       {:gproc, "~> 0.8.0"},
       {:mbcs_rs, "~> 0.1"},
-      {:emojix, "~> 0.3"},
+      # {:emojix, "~> 0.3"},
+      {:emojix, git: "https://github.com/ushitora-anqou/emojix.git"},
       {:google_api_text_to_speech, "~> 0.12.1"},
       {:goth, "~> 1.2.0"}
     ]
