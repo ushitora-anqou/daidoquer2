4a4dbb48ec7180e7b0c393072ac47d260313e87d
Ushitora Anqou <ushitora@anqou.net>
2022-07-11 00:11:53 +0900

    Specify gateway_intents explicitly + add :message_content for Nostrum 0.6.0

--- config/runtime.exs
+++ config/runtime.exs
@@ -6,7 +6,24 @@ config :nostrum,
   token: System.fetch_env!("DISCORD_TOKEN"),
   num_shards: :auto,
   ffmpeg: System.get_env("FFMPEG_PATH", "/usr/bin/ffmpeg"),
-  youtubedl: System.get_env("YOUTUBEDL_PATH", "/usr/bin/youtube-dl")
+  youtubedl: System.get_env("YOUTUBEDL_PATH", "/usr/bin/youtube-dl"),
+  gateway_intents: [
+    :guilds,
+    # :guild_bans,
+    # :guild_emojis,
+    # :guild_integrations,
+    # :guild_webhooks,
+    # :guild_invites,
+    :guild_voice_states,
+    :guild_messages,
+    # :guild_message_reactions,
+    # :guild_message_typing,
+    # :direct_messages,
+    # :direct_message_reactions,
+    # :direct_message_typing,
+    # :guild_scheduled_events,
+    :message_content
+  ]
 
 # config :goth, disabled: true
 
