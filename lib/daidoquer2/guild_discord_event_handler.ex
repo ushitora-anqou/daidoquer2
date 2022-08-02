bc5f0121a03debc725ee9e1d8a45deb1ad6870aa
Ushitora Anqou <ushitora@anqou.net>
2022-08-03 00:47:04 +0900

    Add template_{text,voice}_message in runtime.exs

--- lib/daidoquer2/guild_discord_event_handler.ex
+++ lib/daidoquer2/guild_discord_event_handler.ex
@@ -6,13 +6,15 @@ defmodule Daidoquer2.GuildDiscordEventHandler do
 
   def thread_create(thread_name, user_name, state) do
     Logger.debug("Thread created (#{state.guild_id}) #{thread_name} by #{user_name}")
-    S.cast_bare_message(state.speaker, "#{user_name}さんがスレッド「#{thread_name}」を作りました。")
+    m = voice_template(:created_thread, %{user_name: user_name, thread_name: thread_name})
+    S.cast_bare_message(state.speaker, m)
   end
 
   def i_join(state) do
     Logger.debug("I joined #{state.guild_id}")
     S.cast_enable(state.speaker)
-    S.cast_bare_message(state.speaker, "こんにちは、daidoquer2です。やさしくしてね。")
+    m = voice_template(:i_joined)
+    S.cast_bare_message(state.speaker, m)
   end
 
   def i_leave(state) do
@@ -25,7 +27,8 @@ defmodule Daidoquer2.GuildDiscordEventHandler do
     name = D.display_name_of_user!(state.guild_id, user_id)
     Logger.debug("Name retrieved: #{state.guild_id}: uid=#{user_id} name=#{name}")
 
-    S.cast_bare_message(state.speaker, "#{name}さんが参加しました。")
+    m = voice_template(:joined, %{name: name})
+    S.cast_bare_message(state.speaker, m)
   end
 
   def someone_leave(user_id, state) do
@@ -33,7 +36,8 @@ defmodule Daidoquer2.GuildDiscordEventHandler do
     name = D.display_name_of_user!(state.guild_id, user_id)
     Logger.debug("Name retrieved: #{state.guild_id}: uid=#{user_id} name=#{name}")
 
-    S.cast_bare_message(state.speaker, "#{name}さんが離れました。")
+    m = voice_template(:left, %{name: name})
+    S.cast_bare_message(state.speaker, m)
   end
 
   def start_streaming(user_id, state) do
@@ -41,7 +45,8 @@ defmodule Daidoquer2.GuildDiscordEventHandler do
     name = D.display_name_of_user!(state.guild_id, user_id)
     Logger.debug("Name retrieved: #{state.guild_id}: uid=#{user_id} name=#{name}")
 
-    S.cast_bare_message(state.speaker, "#{name}さんがライブを始めました。")
+    m = voice_template(:started_live, %{name: name})
+    S.cast_bare_message(state.speaker, m)
   end
 
   def stop_streaming(user_id, state) do
@@ -49,41 +54,50 @@ defmodule Daidoquer2.GuildDiscordEventHandler do
     name = D.display_name_of_user!(state.guild_id, user_id)
     Logger.debug("Name retrieved: #{state.guild_id}: uid=#{user_id} name=#{name}")
 
-    S.cast_bare_message(state.speaker, "#{name}さんがライブを終了しました。")
+    m = voice_template(:stopped_live, %{name: name})
+    S.cast_bare_message(state.speaker, m)
   end
 
   def summon_not_from_vc(msg, _state) do
     # The user doesn't belong to VC
-    text_message(msg, "Call from VC")
+    m = text_template(:summon_not_from_vc)
+    text_message(msg, m)
   end
 
   def summon_but_already_joined(msg, _state) do
     # Already joined
-    text_message(msg, "Already joined")
+    m = text_template(:summon_but_already_joined)
+    text_message(msg, m)
   end
 
   def summon(msg, vc_id, state) do
     # Really join
     D.join_voice_channel!(state.guild_id, vc_id)
     channel = D.channel!(vc_id)
-    text_message(msg, "Joined #{channel.name}")
+    m = text_template(:summon, %{channel_name: channel.name})
+    text_message(msg, m)
   end
 
   def unsummon_not_joined(msg, _state) do
-    text_message(msg, "Not yet joined a VC channel")
+    m = text_template(:unsummon_not_joined)
+    text_message(msg, m)
   end
 
   def unsummon_not_from_same_vc(msg, _state) do
     # User does not join the channel
     Logger.debug("'!ddq leave' from another channel")
-    text_message(msg, "Call from the same VC channel")
+    m = text_template(:unsummon_not_from_same_vc)
+    text_message(msg, m)
   end
 
   def unsummon(msg, state) do
+    vm = voice_template(:im_leaving)
+    tm = text_template(:unsummon)
+
     S.stop_speaking_and_clear_message_queue(state.speaker)
-    S.cast_bare_message(state.speaker, "。お相手はdaidoquer2でした。またね。")
+    S.cast_bare_message(state.speaker, vm)
     S.schedule_leave(state.speaker)
-    text_message(msg, "Leaving")
+    text_message(msg, tm)
   end
 
   #####
@@ -95,5 +109,21 @@ defmodule Daidoquer2.GuildDiscordEventHandler do
 
   defp text_message({:interaction, intr}, text) do
     D.text_message_to_interaction(intr, text)
+  end
+
+  defp text_template(key, m \\ nil) do
+    f =
+      Application.fetch_env!(:daidoquer2, :template_text_message)
+      |> Map.fetch!(key)
+
+    f.(m)
+  end
+
+  defp voice_template(key, m \\ nil) do
+    f =
+      Application.fetch_env!(:daidoquer2, :template_voice_message)
+      |> Map.fetch!(key)
+
+    f.(m)
   end
 end
