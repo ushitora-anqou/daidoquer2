9f678ad3ee605f7f7713ad9cae1d42b2c2001fdf
Ushitora Anqou <ushitora@anqou.net>
2022-08-25 21:56:12 +0900

    Bump 0.5.3

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Daidoquer2.MixProject do
   def project do
     [
       app: :daidoquer2,
-      version: "0.5.2",
+      version: "0.5.3",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
