f8c7c4f0696c0bf62775f7aa2854363ef5e34ef5
Ushitora Anqou <ushitora@anqou.net>
2022-08-27 20:46:24 +0900

    Bump Nostrum from 0.6.0 to 0.6.1

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule Daidoquer2.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:nostrum, "~> 0.6.0"},
+      {:nostrum, "~> 0.6.1"},
       {:httpoison, "~> 1.8.0"},
       {:temp, "~> 0.4"},
       # {:emojix, "~> 0.3"},
